d771ebf98aca0205ac9af7955d82b57b2795d161
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2026-09-09 22:40:51 +0530

    Flask learning python code

---
+++ app.py
@@ -0,0 +1,202 @@
+# from flask import Flask, request
+
+# app = Flask(__name__)
+
+# @app.route("/user")
+# def user():
+#     name = request.args.get("name")
+#     age = request.args.get("age")
+
+#     return f"Hello {name}, your age is {age}" #---------------
+
+
+# from flask import Flask, request
+
+# app = Flask(__name__)
+
+# @app.route("/user")
+
+# def user():
+#     name = request.args.get("name")
+#     price = request.args.get("price")
+
+#     return f"{name} your laptop price is {price}".  #-------------
+
+# from flask import Flask, request, render_template
+
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def home():
+#     return render_template("login.html")
+
+
+# @app.route("/login", methods=["POST"])
+# def login():
+
+#     username = request.form.get("username")
+#     password = request.form.get("password")
+
+#     return f"Username: {username}, Password: {password}" # -----------
+
+# from flask import Flask, request, render_template
+
+# app = Flask(__name__)
+
+# @app.route("/profile")
+
+# def profile():
+    # name = "Yogesh Mandawat"
+    # age = 21
+    # skill = "ML Engineer"
+
+
+    # return render_template("profile.html", name = name, age = age, skill = skill) #-----------
+
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+
+# @app.route("/skills")
+# def skills():
+
+#     products = [
+#         "laptop",
+#         "mouse",
+#         "keyboard",
+#         "moniter"
+#     ]
+
+#     return render_template("skills.html", skills=products) #-------------------
+
+# from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+
+
+# @app.route("/college", methods=["GET", "POST"])
+# def college():
+
+#     if request.method == "POST":
+
+#         name = request.form.get("name")
+#         marks = int(request.form.get("marks"))
+
+#         if marks >= 60:
+#             result = "You are eligible for admission."
+#         else:
+#             result = "You are not eligible for admission."
+
+#         return render_template(
+#             "college.html",
+#             name=name,
+#             marks=marks,
+#             result=result
+#         )
+
+#     return render_template("college.html") #--------------------
+
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def home():
+#     return render_template("about.html")
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+##--------------------------------------
+
+# from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+
+# @app.route("/college", methods = ["GET", "POST"])
+
+# def college():
+
+#     if request.method == "POST":
+
+#         name = request.form.get("name")
+#         marks = request.form.get("marks")
+
+#         if not name:
+#             return "Please enter name"
+
+#         if not marks:
+#             return "Please enter marks"
+
+#         marks = int(marks)
+
+#         if(100 < marks < 0):
+#             return "Please Enter Valid Marks"
+
+#         if(marks > 80):
+#             result = "Excellent.."
+        
+#         elif(80 > marks > 60):
+#             result = "Good.."
+
+#         elif(60 > marks > 40):
+#             result = "Poor.."
+
+#         else:
+#             result = "Better luck Next Time.."
+
+#         return render_template(
+#             "college.html",
+#             name = name,
+#             marks = marks,
+#             result = result)
+
+
+#     return render_template("college.html") 
+
+##-----------------------------
+
+# from flask import Flask, render_template, request, url_for, redirect
+
+# app = Flask(__name__)
+
+# @app.route("/college", methods = ["POST", "GET"])
+
+# def college():
+
+#     if request.method == "POST":
+
+#         name = request.form.get("name")
+#         marks = request.form.get("marks")
+
+#         marks = int(marks)
+
+#         if not name:
+#             return "Name Required"
+
+#         if not marks:
+#             return "Marks Required"
+
+#         if(0 > marks > 100):
+#             return "Please Enter Valid Marks"
+        
+#         #result = "You are passed" # when i used render templates..
+#         #return render_template("college.html", name = name, marks=marks, result = result) 
+    
+#         # it will directly open the new page but function name is required
+#         return redirect("result")
+#     return render_template("college.html")
+
+# @app.route("/result")
+# def result():
+#     return " Your result will be shown here...."
+
+# @app.route("/yogesh") # its for checking the redirect method
+# def yogesh():
+#     return "Hey this is yogesh bhau mandawat"
+
+# from flask import Flask, render_template, redirect, url_for, request '
